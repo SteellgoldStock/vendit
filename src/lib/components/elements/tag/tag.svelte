7a6e60e23efc36a78b6d0b7d215e846212b53bf1
Gaëtan H <steellgold0@gmail.com>
2022-09-25 13:07:49 +0200

    tag
    todo: special (check figma)

---
+++ src/lib/components/elements/tag/tag.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  export let text = "Hello world";
+  export let type: "primary" | "secondary" | "success" | "danger" | "warning" | "info" | "light" | "dark" = "primary";
+</script>
+
+<span class="tag {type}">
+  {text}
+</span>
+
+<style lang="scss">
+  @import "../../../scss/colors.scss";
+  @import "../../../scss/variables.scss";
+
+  .tag {
+    background: $white-300;
+    color: $white-900;
+    padding: 5px 10px;
+    border-radius: 5px;
+    font-size: 0.8rem;
+    font-weight: 600;
+  }
+
+  .primary {
+    background: $primary;
+    color: $white;
+  }
+
+  .secondary {
+    background: $secondary;
+    color: $white;
+  }
+
+  .success {
+    background: $green;
+    color: $white;
+  }
+
+  .danger {
+    background: $red;
+    color: $white;
+  }
+
+  .warning {
+    background: $orange;
+    color: $white;
+  }
+
+  .info {
+    background: $blue;
+    color: $white;
+  }
+</style>
