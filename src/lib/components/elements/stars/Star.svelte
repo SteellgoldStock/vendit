70e0d9efc64a4cb9ba25d143956a80977350522b
Gaëtan H <steellgold0@gmail.com>
2022-09-25 12:04:36 +0200

    star component (half + disabled)

---
+++ src/lib/components/elements/stars/Star.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let half: boolean = false;
+  export let disabled: boolean = false;
+</script>
+
+<i class="fa-solid fa-star {half ? "half" : ""} {disabled ? "disabled" : ""}"></i>
+
+<style lang="scss">
+  @import "../../../scss/colors.scss";
+  
+  i.half {
+    color: $yellow;
+    background: linear-gradient(to right, $yellow 50%, $white-600 50%);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+
+  i.disabled {
+    color: $white-600;
+  }
+
+  i {
+    color: $yellow;
+  }
+</style>
---
+++ src/lib/components/elements/stars/index.ts
@@ -0,0 +1,1 @@
+export { default as Stars } from './stars.svelte';
