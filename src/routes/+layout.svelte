8463170841988c2f362b1e0dfe49148195af0066
Gaëtan H <steellgold0@gmail.com>
2022-09-25 11:14:07 +0200

    layout for pages

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { Sidebar } from "$lib/components/layouts/sidebar";
+</script>
+
+<Sidebar />
+
+<div class="content">
+  <slot></slot>
+</div>
+
+<style lang="scss">
+  @import "../lib/scss/colors.scss";
+  @import "../lib/scss/variables.scss";
+
+  .content {
+    @media (min-width: $breakpoint-md) {
+      margin-left: 310px;
+    }
+  }
+</style>
