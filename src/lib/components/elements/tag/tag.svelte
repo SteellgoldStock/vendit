d639c488afc5296d7dd1c3dd6eb27fc89f5c4a0c
Gaëtan H <steellgold0@gmail.com>
2022-09-25 13:15:39 +0200

    edit

--- src/lib/components/elements/tag/tag.svelte
+++ src/lib/components/elements/tag/tag.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   export let text = "Hello world";
-  export let type: "primary" | "secondary" | "success" | "danger" | "warning" | "info" | "light" | "dark" = "primary";
+  export let type: "primary" | "secondary" | "success" | "danger" | "warning" | "info" | "light" | "dark" | "special" = "primary";
 </script>
 
 <span class="tag {type}">
-  {text}
+  <div class="text">
+    {text}
+  </div>
 </span>
 
 <style lang="scss">
@@ -17,7 +19,7 @@
     padding: 5px 10px;
     border-radius: 5px;
     font-size: 0.8rem;
-    font-weight: 600;
+    font-weight: 800;
   }
 
   .primary {
@@ -48,5 +50,29 @@
   .info {
     background: $blue;
     color: $white;
+  }
+
+  .light {
+    background: $white-100;
+    color: $white-900;
+    border: 2px solid $white-300;
+  }
+
+  .dark {
+    background: $white-900;
+    color: $white;
+  }
+
+  .special {
+    // text gradiant color
+    background: linear-gradient(180deg, #465F85 0%, #3A496D 100%);
+
+    .text {
+      // text gradiant color
+      background: linear-gradient(92.68deg, #EAE692 1.83%, #94DF9B 32.1%, #9BF9E6 62.37%, #79C3ED 92.64%);
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+      font-weight: 900;
+    }
   }
 </style>
