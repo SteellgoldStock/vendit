4c812621ebc01d872a5326e97a45cfdf8b6b1241
Gaëtan H <steellgold0@gmail.com>
2022-09-25 13:07:39 +0200

    add progress bar
    TODO: Animation

---
+++ src/lib/components/elements/progress-bar/progress-bar.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let progress: number = 0;
+</script>
+
+<div class="progressbar">
+  <div class="progress" style="width: {progress}%"></div>
+</div>
+
+<style type="scss">
+  @import "../../../scss/colors.scss";
+  @import "../../../scss/variables.scss";
+
+  .progressbar {
+    width: 100%;
+    height: 10px;
+    background: #455E84;
+    overflow: hidden;
+
+    .progress {
+      height: 100%;
+      background: $yellow;
+    }
+
+    transition: width 0.5s ease;
+  }
+</style>
