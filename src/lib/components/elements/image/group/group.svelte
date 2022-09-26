8de52e552971f65db849284aa873cccefe6f0dd4
Gaëtan H <steellgold0@gmail.com>
2022-09-26 18:10:43 +0200

    add group and image component

---
+++ src/lib/components/elements/image/group/group.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let alt: string = "Image";
+</script>
+
+<div class="group">
+  <slot></slot>
+</div>
+
+<style lang="scss">
+  @import "../../../../../lib/scss/colors.scss";
+  @import "../../../../../lib/scss/variables.scss";
+
+  .group {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+  }
+</style>
---
+++ src/lib/components/elements/image/image.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  export let alt: string = "Image";
+  export let src: string = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/404_File_not_found.png/220px-404_File_not_found.png";
+</script>
+
+<div class="image">
+  <img src={src} alt={alt} />
+</div>
+
+<style lang="scss">
+  @import "../../../../../lib/scss/colors.scss";
+  @import "../../../../../lib/scss/variables.scss";
+
+  .image {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    background: $white;
+    border-radius: 10px;
+    overflow: hidden;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+</style>
