9847ca009c269e7e6fde3a16093a161405dfca80
Gaëtan H <steellgold0@gmail.com>
2022-09-29 12:31:03 +0200

    add css to sellers section (1/2)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,21 +16,22 @@
 <hr class="separator">
 
 <div class="sellers">
-  <div class="row">
-    <div class="left">
-      <div class="title">
-        <h2 class="title">DISCOVER OUR SELLERS</h2>
-        <p class="subtitle">There are many more!</p>
-      </div>
+  <div class="rows">
+    <div class="text">
+      <h2>Sellers</h2>
+      <p>Find your favorite PocketMine plugin sellers here</p>
     </div>
-
-    <div class="right">
-      <div class="sellers-imgs">
-        <img src="https://cdn.discordapp.com/avatars/504392983244832780/ed108638e6cba46a3498bc6da92158f6.webp?size=256" alt="Gaëtan">
-        <img src="https://cdn.discordapp.com/avatars/533306853317279773/6eac2d06855a52de73bc48343a00a2c1.png?size=256" alt="Romain">
-        <img src="https://cdn.discordapp.com/avatars/371298344921726978/2e12f0a06b95725bf492c1a22e43b4b6.png?size=256" alt="Loupio">
-        <img src="https://cdn.discordapp.com/avatars/233351173665456129/a_cee03ee0f97ee3c550840d584bc55c21.png?size=256" alt="Bluzzi">
-        <img src="https://cdn.discordapp.com/avatars/162633729703936001/c68325b9d4509bcd7e4a56a32dd6ff30.png?size=256" alt="King">
+    <div class="images">
+      <div class="col1">
+        <img src="https://cdn.discordapp.com/avatars/504392983244832780/ed108638e6cba46a3498bc6da92158f6.webp?size=256" height="150px"alt="Gaëtan">
+      </div>
+      <div class="col2">
+        <img src="https://cdn.discordapp.com/avatars/533306853317279773/6eac2d06855a52de73bc48343a00a2c1.png?size=256" height="100px" alt="Romain">
+        <img src="https://cdn.discordapp.com/avatars/371298344921726978/2e12f0a06b95725bf492c1a22e43b4b6.png?size=256" height="100px" alt="Loupio">
+      </div>
+      <div class="col3">
+        <img src="https://cdn.discordapp.com/avatars/233351173665456129/a_cee03ee0f97ee3c550840d584bc55c21.png?size=256" height="90px" alt="Bluzzi">
+        <img src="https://cdn.discordapp.com/avatars/162633729703936001/c68325b9d4509bcd7e4a56a32dd6ff30.png?size=256" height="100px" alt="King">
       </div>
     </div>
   </div>
@@ -73,65 +74,39 @@
     margin-top: -8px;
     background: linear-gradient(180deg, #2B324E 35.24%, rgba(58, 70, 118, 0.9) 100%);
 
-    .title {
+    .rows {
       display: flex;
-      flex-direction: column;
+      justify-content: space-between;
       align-items: center;
-      text-align: center;
-      color: $white;
 
-      h2 {
-        text-transform: uppercase;
-        font-weight: 900;
-        font-size: 2rem;
+      padding: 50px 20px;
+      margin: 0 auto;
+      max-width: 1200px;
 
-        @media (max-width: $breakpoint-md) {
-          font-size: 1.5rem;
+      .text {
+        h2 {
+          font-size: 2rem;
+          font-weight: 900;
+          margin: 0;
+          color: $white;
+        }
+
+        p {
+          font-size: 1.2rem;
+          font-weight: 500;
+          margin: 0;
+          color: $white-600;
         }
       }
-      
-      p {
-        font-size: 1rem;
-        font-weight: 400;
-        margin-top: -15px;
 
-        @media (max-width: $breakpoint-md) {
-          width: 250px;
-        }
-      }
-    }
-
-    .images {
-      margin-top: -10px;
-      
-      .top {
-        display: flex;
-        justify-content: center;
-        gap: 10px;
-        margin-top: 20px;
-        height: 100px;
-      }
-
-      .bottom {
-        display: flex;
-        justify-content: center;
-        gap: 10px;
-        margin-top: 10px;
-        height: 65px;
-        opacity: 0.5;
-      }
-      
-      padding-bottom: 50px;
-    }
-
-    @media (max-width: $breakpoint-md) {
       .images {
-        .top {
-          height: 40px;
-        }
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
 
-        .bottom {
-          height: 40px;
+        img {
+          border-radius: 10px;
+          object-fit: cover;
         }
       }
     }
