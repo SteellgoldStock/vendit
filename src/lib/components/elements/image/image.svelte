ffdad7564b2b85b704d942c1981aee114557b3d6
Gaëtan H <steellgold0@gmail.com>
2022-09-29 18:52:24 +0200

    unused component

--- src/lib/components/elements/image/image.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-  export let alt: string = "Image";
-  export let src: string = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/404_File_not_found.png/220px-404_File_not_found.png";
-</script>
-
-<img src={src} alt={alt} />
