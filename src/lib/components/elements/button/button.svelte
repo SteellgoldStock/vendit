6e8ff3ea68bda756c07ffaaf26392fff8d86e8c9
Gaëtan H <steellgold0@gmail.com>
2022-10-01 15:34:15 +0000

    add color variables to buttons

--- src/lib/components/elements/button/button.svelte
+++ src/lib/components/elements/button/button.svelte
@@ -2,8 +2,9 @@
   interface ButtonInfo {
     type: "button" | "link";
     text: string;
+    color?: "blue" | "red" | "green" | "yellow" | "purple" | "orange" | "white";
     href?: string;
-    icon?: string; // font awesome icon
+    icon?: string;
   }
 
   export let buttonInfo: ButtonInfo;
@@ -15,12 +16,12 @@
 </script>
 
 {#if buttonInfo.type === "button"}
-  <button class="button">
+  <button class="button {buttonInfo.color}">
     {@html icon ?? ""}
     {buttonInfo.text}
   </button>
 {:else if buttonInfo.type === "link"}
-  <a href={buttonInfo.href} class="button">
+  <a href={buttonInfo.href} class="button {buttonInfo.color}">
     {@html icon ?? ""}
     {buttonInfo.text}
   </a>
@@ -39,6 +40,35 @@
     border: none;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     transition: all 0.2s ease-in-out;
+  }
+
+  .blue {
+    background-color: $blue;
+  }
+
+  .red {
+    background-color: $red;
+  }
+
+  .green {
+    background-color: $green;
+  }
+
+  .yellow {
+    background-color: $yellow;
+  }
+
+  .purple {
+    background-color: $purple;
+  }
+
+  .orange {
+    background-color: $orange;
+  }
+
+  .white {
+    background-color: $white;
+    color: $white-900;
   }
 
   .button:hover {
