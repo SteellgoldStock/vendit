0836463214c69caa843a544bf3d7b86aa08f3313
Gaëtan H <steellgold0@gmail.com>
2022-10-03 19:12:09 +0200

    404 (Lang)

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,52 +1,9 @@
 <script lang="ts">
   import { page } from '$app/stores';
   import { Button } from '$lib/components/elements/button';
+  import { getLang } from "$lib/utils/lang/lang";
 
   let status: number = $page.status;
-  let message: string = "";
-
-  const statusText = {
-    400: "Bad Request",
-    401: "Unauthorized",
-    403: "Forbidden",
-    404: "Not Found",
-    405: "Method Not Allowed",
-    406: "Not Acceptable",
-    408: "Request Timeout",
-    409: "Conflict",
-    410: "Gone",
-    411: "Length Required",
-    412: "Precondition Failed",
-    413: "Payload Too Large",
-    414: "URI Too Long",
-    415: "Unsupported Media Type",
-    416: "Range Not Satisfiable",
-    417: "Expectation Failed",
-    418: "I'm a teapot",
-    421: "Misdirected Request",
-    422: "Unprocessable Entity",
-    423: "Locked",
-    424: "Failed Dependency",
-    425: "Too Early",
-    426: "Upgrade Required",
-    428: "Precondition Required",
-    429: "Too Many Requests",
-    431: "Request Header Fields Too Large",
-    451: "Unavailable For Legal Reasons",
-    500: "Internal Server Error",
-    501: "Not Implemented",
-    502: "Bad Gateway",
-    503: "Service Unavailable",
-    504: "Gateway Timeout",
-    505: "HTTP Version Not Supported",
-    506: "Variant Also Negotiates",
-    507: "Insufficient Storage",
-    508: "Loop Detected",
-    510: "Not Extended",
-    511: "Network Authentication Required",
-  } as { [key: number]: string };
-
-  $: message = statusText[status] || "Unknown Error";
 </script>
 
 <svelte:head>
@@ -55,12 +12,12 @@
 
 <div class="error">
   <h1>{status}</h1>
-  <p>{message}</p>
+  <p>{ getLang().error_page.errors[status] }</p>
   
   <div class="button">
     <Button buttonInfo={{
       type: "link",
-      text: "Go back to homepage",
+      text: getLang().error_page.button,
       icon: "fa-solid fa-arrow-left",
       href: "/"
     }} />
