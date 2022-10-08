bc4c49a3d652330542bfeca6f5f4f6e20de3e214
Gaëtan H <steellgold0@gmail.com>
2022-10-08 19:30:12 +0200

    Revert "Some change"
    This reverts commit 879d3501ecb023435646fd37d3a71b41ac892179.

--- src/routes/seller/[id]/+page.svelte
+++ src/routes/seller/[id]/+page.svelte
@@ -97,3 +97,119 @@
 <svelte:head>
   <title>Seller ・ {seller.name}</title>
 </svelte:head>
+
+<section class="presentation">
+  <div class="presentation__avatar">
+    <img src={seller.avatarURL} alt={seller.name} />
+  </div>
+
+  <div class="presentation__content">
+    <h1 class="presentation__content__name">{seller.name}</h1>
+    <p class="presentation__content__description">{seller.description}</p>
+  </div>
+
+  <div class="presentation__actions">
+    <button class="presentation__actions__button">Follow</button>
+    <button class="presentation__actions__button">Message</button>
+  </div>
+
+  <div class="presentation__stats">
+    <div class="presentation__stats__stat">
+      <h1 class="presentation__stats__stat__value">0</h1>
+      <p class="presentation__stats__stat__label">Followers</p>
+    </div>
+
+    <div class="presentation__stats__stat">
+      <h1 class="presentation__stats__stat__value">0</h1>
+      <p class="presentation__stats__stat__label">Following</p>
+    </div>
+
+    <div class="presentation__stats__stat">
+      <h1 class="presentation__stats__stat__value">0</h1>
+      <p class="presentation__stats__stat__label">Products</p>
+    </div>
+
+    <div class="presentation__stats__stat">
+      <h1 class="presentation__stats__stat__value">0</h1>
+      <p class="presentation__stats__stat__label">Comments</p>
+    </div>
+</section>
+
+<style lang="scss">
+  .presentation {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+    padding: 1rem;
+
+    &__avatar {
+      width: 10rem;
+      height: 10rem;
+      border-radius: 50%;
+      overflow: hidden;
+
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    }
+
+    &__content {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 0.5rem;
+
+      &__name {
+        font-size: 1.5rem;
+        font-weight: 600;
+      }
+
+      &__description {
+        font-size: 1rem;
+        font-weight: 400;
+        text-align: center;
+      }
+    }
+
+    &__actions {
+      display: flex;
+      gap: 0.5rem;
+
+      &__button {
+        padding: 0.5rem 1rem;
+        border: none;
+        border-radius: 0.5rem;
+        background-color: #2f3136;
+        color: #fff;
+        font-size: 1rem;
+        font-weight: 600;
+        cursor: pointer;
+      }
+    }
+
+    &__stats {
+      display: flex;
+      gap: 1rem;
+
+      &__stat {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 0.5rem;
+
+        &__value {
+          font-size: 1.5rem;
+          font-weight: 600;
+        }
+
+        &__label {
+          font-size: 1rem;
+          font-weight: 400;
+        }
+      }
+    }
+  }
+</style>
