885b43cdee276972fde7c25e57ed88177603b743
Gaëtan H <steellgold0@gmail.com>
2022-10-08 12:56:38 +0200

    Add slider

---
+++ src/lib/components/elements/form/slider/slider.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  interface Slider {
+    min: number;
+    max: number;
+    step: number;
+  }
+
+  export let slider: Slider;
+
+  let value = 0;
+</script>
+
+<div class="slider">
+  <input type="range" min={slider.min ?? 0} max={slider.max ?? 5} step={slider.step ?? 1} bind:value={value}>
+</div>
+
+<style lang="scss">
+  @import '../../../../scss/colors.scss';
+  @import '../../../../scss/variables.scss';
+
+  .slider {
+    width: 100%;
+
+    input[type=range] {
+      -webkit-appearance: none;
+      width: 100%;
+      height: 0.5rem;
+      border-radius: 0.5rem;
+      background: $secondary;
+      outline: none;
+      opacity: 0.7;
+      -webkit-transition: .2s;
+      transition: opacity .2s;
+
+      // edit the slider's thumb appearance to star
+      &::-webkit-slider-thumb {
+        -webkit-appearance: none;
+        width: 1rem;
+        height: 1rem;
+        border-radius: 50%;
+        background: white;
+        cursor: pointer;
+      }
+    }
+  }
+</style>
